b899a1ac94db736424ad4827dacd6b6fe2bc4c56
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-08-20 08:53:23 -0400

    imported app into repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import WeatherSearch from "./WeatherSearch.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <h2>
+        <WeatherSearch />
+      </h2>
+    </div>
+  );
+}
